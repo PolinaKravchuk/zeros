8ec4e14538d18cc3e1444ca3403e58bd87e4ea2e
Polina Kravchuk <Polina Kravchuk>
2018-02-26 00:24:52 +0300

    D:/_programs/Git/5 /2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 module.exports = function getZerosCount(number) {
-  var ans = number / 5;
-  return ans;
+  var kol_5 = number / 5;
+  var kol_2 = number / 2;
+
+  if (kol_2 < kol_5) return kol_2;
+  else return kol_5;
   
 }
