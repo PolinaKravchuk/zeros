1ecef0c3cc455292775a5444c6039f97f3bd9c4e
Polina Kravchuk <Polina Kravchuk>
2018-02-26 00:48:14 +0300

    2 - %5 %2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 module.exports = function getZerosCount(number) {
-  var kol = 0;
-
+  var kol_5 = 0,
+  	  kol_2 = 0;
+  var t5, t2;
   for (var i = 0; i < number; i++ ){
-  	if(i % 10 == 0 || i % 5 == 0 || i % 2 == 0) kol++;
+  	if( i % 5 == 0 ) kol_5++;
+  	t5 = i;
+  	while(true) {
+	  	t5 = t5 / 5;
+	  	if (t5 % 5 == 0) kol_5++;
+	  	else break;
+ 	 }
+
+ 	if( i % 2 == 0 ) kol_2++;
+  	t2 = i;
+  	while(true) {
+	  	t2 = t2 / 5;
+	  	if (t2 % 5 == 0) kol_2++;
+	  	else break;
+ 	 }
   }
-  return kol;
+
+  if(kol_5 < kol_2) return kol_5;
+  else return kol2;
 }
